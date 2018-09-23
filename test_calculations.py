6f32889a1c0bca0e4ab64b497f32a7d5bc05b958
tomoya_yamada <tomoyaaa0310@gmail.com>
2018-09-23 02:22:29 +0000

    testを修正

--- test_calculations.py
+++ test_calculations.py
@@ -9,7 +9,7 @@ class CalculationsTest(unittest.TestCase):
 
     def test_divide_contain_zero(self):
         # 結果が1であるため修正
-        self.assertEqual(calculations.divide(0, 1), 1)
+        self.assertEqual(calculations.divide(0, 1), 0)
 
 if __name__ == "__main__":
     unittest.main()
