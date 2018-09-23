65a116545907db7bca9392d4adb18dfd75af418c
tomoya_yamada <tomoyaaa0310@gmail.com>
2018-09-23 03:35:56 +0000

    django追加

---
+++ cafe/apps/menu/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+TEA_KINDS = (
+    ("english", "英国紅茶"),
+    ("chinese", "中国茶"),
+    ("japanese", "日本茶"),
+)
+
+class TeaManager(models.Manager):
+    def count_each_kind(self):
+        result = self.values_list("kind").annotate(count=models.Count("kind"))
+        return dict(result)
+
+class Tea(models.Model):
+    objects = TeaManager()
+
+    name = models.CharField("名称", max_length=255)
+    kind = models.CharField("種類", max_length=255, choices=TEA_KINDS)
